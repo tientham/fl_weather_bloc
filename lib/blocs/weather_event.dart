ef1bc1fc77d876bfd8ca3d018aa486d29fc61a84
Minh Tien TO (TienTham) <tien.tominh@gmail.com>
2021-03-26 23:20:32 +0100

    refresh indicator

--- lib/blocs/weather_event.dart
+++ lib/blocs/weather_event.dart
@@ -12,3 +12,13 @@ class WeatherRequested extends WeatherEvent {
   @override
   List<Object> get props => [city];
 }
+
+class WeatherRefreshRequested extends WeatherEvent {
+
+  final String city;
+
+  const WeatherRefreshRequested({required this.city});
+
+  @override
+  List<Object?> get props => [city];
+}
--- lib/widgets/weather.dart
+++ lib/widgets/weather.dart
@@ -1,9 +1,24 @@
+import 'dart:async';
+
 import 'package:fl_weather_bloc/blocs/blocs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fl_weather_bloc/widgets/widgets.dart';
 
-class Weather extends StatelessWidget {
+class Weather extends StatefulWidget {
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+
+  late Completer<void> _refreshCompleter;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshCompleter = Completer<void>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +38,13 @@ class Weather extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: BlocBuilder<WeatherBloc, WeatherState>(
+        child: BlocConsumer<WeatherBloc, WeatherState>(
+          listener: (context, state) {
+            if (state is WeatherLoadSuccess) {
+              _refreshCompleter.complete();
+              _refreshCompleter = Completer();
+            }
+          },
           builder: (context, state) {
             if (state is WeatherInitial) {
               return Center(
@@ -40,24 +61,30 @@ class Weather extends StatelessWidget {
             if (state is WeatherLoadSuccess) {
               final weather = state.weather;
 
-              return ListView(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 100.0),
-                    child: Center(
-                      child: Location(location: weather.location),
+              return RefreshIndicator(
+                onRefresh: () {
+                  BlocProvider.of<WeatherBloc>(context).add(WeatherRefreshRequested(city: state.weather.location));
+                  return _refreshCompleter.future;
+                },
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 100.0),
+                      child: Center(
+                        child: Location(location: weather.location),
+                      ),
                     ),
-                  ),
-                  Center(
-                    child: LastUpdated(dateTime: weather.lastUpdated),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50.0),
-                    child: Center(
-                      child: CombinedWeatherTemperature(weather: weather),
+                    Center(
+                      child: LastUpdated(dateTime: weather.lastUpdated),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 50.0),
+                      child: Center(
+                        child: CombinedWeatherTemperature(weather: weather),
+                      ),
+                    ),
+                  ],
+                ),
               );
             }
 
